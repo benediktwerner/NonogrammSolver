73eeecdeb4b8c6b461e58597a0ed5f929d6ea4fc
Benedikt Werner <1benediktwerner@gmail.com>
2019-01-29 21:05:42 +0100

    Start other solver without z3

--- solver.py
+++ solver.py
@@ -2,8 +2,8 @@
 
 import os
 import sys
-import itertools
-from z3 import Int, Bool, And, Or, Not, If, Implies, sat, Solver, simplify
+
+UNKNOWN, FREE, FILLED = range(3)
 
 
 class Nonogramm:
@@ -12,109 +12,95 @@ class Nonogramm:
         self.height = height
         self.rows = tuple(tuple(x) for x in rows)
         self.cols = tuple(tuple(x) for x in cols)
-        self.grid = self.__gen_grid()
+        self.grid = [[UNKNOWN]*height for _ in range(width)]
 
         self.dimensions = (width, height)
-        self.__possible_lines = {}
 
-    def __gen_grid(self):
-        grid = []
+    def __get(self, axis, line_index, cell_index):
+        if axis == 0:
+            return self.grid[line_index][cell_index]
+        return self.grid[cell_index][line_index]
+
+    def __set(self, axis, line_index, cell_index, value):
+        if axis == 0:
+            self.grid[line_index][cell_index] = value
+        else:
+            self.grid[cell_index][line_index] = value
+
+    def __prefill(self, axis, line_index):
+        hints = (self.cols if axis == 0 else self.rows)[line_index]
+        space = self.dimensions[1 - axis] - sum(hints) - len(hints) + 1
+        if space < max(hints):
+            total = 0
+            for hint in hints:
+                if hint > space:
+                    for i in range(total+space, total+hint):
+                        self.__set(axis, line_index, i, FILLED)
+                total += hint + 1
+
+    def solve(self):
         for x in range(self.width):
-            grid.append([Bool("{}_{}".format(x, y)) for y in range(self.height)])
-        return grid
+            self.__prefill(0, x)
 
-    def __grid(self, axis, line, cell):
-        return self.grid[line if axis == 0 else cell][line if axis == 1 else cell]
+        for y in range(self.height):
+            self.__prefill(1, y)
 
-    def __generate_line_constraints(self, axis, line_index):
-        variables = []
-        line = (self.cols if axis == 0 else self.rows)[line_index]
-        breakpoints = [line[0]]
+    def print_grid(self):
+        for y in range(self.height):
+            for x in range(self.width):
+                print((".", "X", "#")[self.grid[x][y]], end="")
+            print()
 
-        for x in line[1:]:
-            breakpoints.append(breakpoints[-1] + 1 + x)
+    @staticmethod
+    def from_file(filename):
+        index = -1
+        rows = []
+        cols = []
 
-        for i in range(self.dimensions[1 - axis]):
-            variables.append(Int("{}_{}_{}".format(axis, line_index, i)))
-            curr_cell = self.__grid(axis, line_index, i)
+        with open(filename) as f:
+            try:
+                for line in f:
+                    line = line.strip()
 
-            if i == 0:
-                yield variables[i] == If(curr_cell, 1, 0)
-            else:
-                prev_cell = self.__grid(axis, line_index, i-1)
-                yield variables[i] == variables[i-1] + If(Or(curr_cell, prev_cell), 1, 0)
-                yield And(prev_cell, Not(curr_cell)) == Or(*(variables[i-1] == b for b in breakpoints))
+                    if not line or line[0] == "#":
+                        continue
 
-        yield variables[-1] == breakpoints[-1] + If(curr_cell, 0, 1)
+                    if index < 0:
+                        width, height = map(int, line.split(" "))
+                    elif index < height:
+                        rows.append([int(x) for x in line.split(" ")])
+                    elif index < height + width:
+                        cols.append([int(x) for x in line.split(" ")])
+                    else:
+                        break
 
-    def __gen_axis_constraints(self, axis):
-        for i in range(self.dimensions[axis]):
-            yield from self.__generate_line_constraints(axis, i)
+                    index += 1
+            except ValueError:
+                return None
 
-    def gen_constraints(self):
-        return itertools.chain(self.__gen_axis_constraints(0), self.__gen_axis_constraints(1))
-
-
-def load_file(filename):
-    index = -1
-    rows = []
-    cols = []
-
-    with open(filename) as f:
-        try:
-            for line in f:
-                line = line.strip()
-
-                if not line or line[0] == "#":
-                    continue
-
-                if index < 0:
-                    width, height = map(int, line.split(" "))
-                elif index < height:
-                    rows.append([int(x) for x in line.split(" ")])
-                elif index < height + width:
-                    cols.append([int(x) for x in line.split(" ")])
-                else:
-                    break
-
-                index += 1
-        except ValueError:
+        if len(rows) != height or len(cols) != width:
             return None
-
-    if len(rows) != height or len(cols) != width:
-        return None
-    return Nonogramm(width, height, rows, cols)
+        return Nonogramm(width, height, rows, cols)
 
 
-if len(sys.argv) != 2 or sys.argv[1] in ("-h", "help"):
-    print("Usage:", sys.argv[0], "NONOGRAMM-FILE")
-    exit(1)
-elif not os.path.isfile(sys.argv[1]):
-    print("'{}' is not a valid file".format(sys.argv[1]))
-    exit(1)
+def main():
+    if len(sys.argv) != 2 or sys.argv[1] in ("-h", "help"):
+        print("Usage:", sys.argv[0], "NONOGRAMM-FILE")
+        exit(1)
+    elif not os.path.isfile(sys.argv[1]):
+        print("'{}' is not a valid file".format(sys.argv[1]))
+        exit(1)
 
-nonogramm = load_file(sys.argv[1])
+    nonogramm = Nonogramm.from_file(sys.argv[1])
 
-if not nonogramm:
-    print("'{}' doesn't contain a valid nonogramm".format(sys.argv[1]))
-    exit(2)
+    if nonogramm is None:
+        print("'{}' doesn't contain a valid nonogramm".format(sys.argv[1]))
+        exit(2)
 
-solver = Solver()
+    print("Solving...")
+    nonogramm.solve()
+    nonogramm.print_grid()
 
-print("Generating constraints ...")
 
-for constraint in nonogramm.gen_constraints():
-    solver.add(simplify(constraint))
-
-print("Solving...")
-
-if solver.check() == sat:
-    print("Solved:")
-    model = solver.model()
-
-    for y in range(nonogramm.height):
-        for x in range(nonogramm.width):
-            print("#" if model[nonogramm.grid[x][y]] else ".", end="")
-        print()
-else:
-    print("Unsolvable!")
+if __name__ == "__main__":
+    main()
