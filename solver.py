e8614956738cfea86c022995a920f90fb2f2fdb9
Benedikt Werner <1benediktwerner@gmail.com>
2019-01-29 06:35:59 +0100

    Massive performance increase when generating constraints

--- solver.py
+++ solver.py
@@ -3,7 +3,7 @@
 import os
 import sys
 import itertools
-from z3 import Bool, And, Or, If, sat, Solver, simplify
+from z3 import Int, Bool, And, Or, Not, If, Implies, sat, Solver, simplify
 
 
 class Nonogramm:
@@ -20,38 +20,36 @@ class Nonogramm:
     def __gen_grid(self):
         grid = []
         for x in range(self.width):
-            grid.append([Bool("({},{})".format(x, y)) for y in range(self.height)])
+            grid.append([Bool("{}_{}".format(x, y)) for y in range(self.height)])
         return grid
 
-    def __generate_possible(self, values, size):
-        if (values, size) in self.__possible_lines:
-            return self.__possible_lines[(values, size)]
+    def __grid(self, axis, line, cell):
+        return self.grid[line if axis == 0 else cell][line if axis == 1 else cell]
 
-        result = []
-        space = size - sum(values) - len(values) + 1
+    def __generate_line_constraints(self, axis, line_index):
+        variables = []
+        line = (self.cols if axis == 0 else self.rows)[line_index]
+        breakpoints = [line[0]]
 
-        for perm in permutations(len(values) + 1, space):
-            pos = []
-            for i, x in enumerate(perm):
-                if i != 0:
-                    if i > 1:
-                        pos.append(False)
-                    pos += [True] * values[i-1]
-                pos += [False] * x
-            result.append(pos)
+        for x in line[1:]:
+            breakpoints.append(breakpoints[-1] + 1 + x)
 
-        self.__possible_lines[(values, size)] = result
-        return result
+        for i in range(self.dimensions[1 - axis]):
+            variables.append(Int("{}_{}_{}".format(axis, line_index, i)))
+            curr_cell = self.__grid(axis, line_index, i)
+
+            if i == 0:
+                yield variables[i] == If(curr_cell, 1, 0)
+            else:
+                prev_cell = self.__grid(axis, line_index, i-1)
+                yield variables[i] == variables[i-1] + If(Or(curr_cell, prev_cell), 1, 0)
+                yield And(prev_cell, Not(curr_cell)) == Or(*(variables[i-1] == b for b in breakpoints))
+
+        yield variables[-1] == breakpoints[-1] + If(curr_cell, 0, 1)
 
     def __gen_axis_constraints(self, axis):
-        for a in range(self.dimensions[axis]):
-            valid = False
-            for pos in self.__generate_possible(self.rows[a] if axis else self.cols[a], self.dimensions[1-axis]):
-                equal = True
-                for b in range(self.dimensions[1-axis]):
-                    equal = And(equal, (self.grid[b if axis else a][a if axis else b] == pos[b]))
-                valid = Or(valid, equal)
-            yield simplify(valid)
+        for i in range(self.dimensions[axis]):
+            yield from self.__generate_line_constraints(axis, i)
 
     def gen_constraints(self):
         return itertools.chain(self.__gen_axis_constraints(0), self.__gen_axis_constraints(1))
@@ -88,19 +86,6 @@ def load_file(filename):
     return Nonogramm(width, height, rows, cols)
 
 
-def permutations(buckets, balls):
-    if buckets == 1:
-        return [[balls]]
-    if balls == 0:
-        return [[0]*buckets]
-
-    result = []
-    for x in range(balls+1):
-        for perm in permutations(buckets - 1, balls - x):
-            result.append([x] + perm)
-    return result
-
-
 if len(sys.argv) != 2 or sys.argv[1] in ("-h", "help"):
     print("Usage:", sys.argv[0], "NONOGRAMM-FILE")
     exit(1)
@@ -119,7 +104,7 @@ solver = Solver()
 print("Generating constraints ...")
 
 for constraint in nonogramm.gen_constraints():
-    solver.add(constraint)
+    solver.add(simplify(constraint))
 
 print("Solving...")
 
